9202fdfe185bf0abbc1332c87972889fa3dacd46
LAPTOP-5C4O9PKD\Kamil <kamil.kulerz@gmail.com>
2021-11-28 22:21:39 +0100

    add order form

---
+++ orders_app/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from django import forms
+from .models import Order, ORDER_STATUSES
+
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = ['status']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['status'].widget.attrs.update(
+            {'onchange': 'form.submit();'})
