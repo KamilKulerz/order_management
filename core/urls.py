9a7e7a0f0a508dbd91a4fdb57b57a727e1b2da01
LAPTOP-5C4O9PKD\Kamil <kamil.kulerz@gmail.com>
2021-12-12 18:18:53 +0100

    rename main app

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-"""order_management URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -20,4 +20,5 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orders_app.urls')),
+    path('api/', include('api.urls')),
 ]
