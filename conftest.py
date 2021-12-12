9f0218e1788634763153cbf3ca59e3cfa5a3fc03
LAPTOP-5C4O9PKD\Kamil <kamil.kulerz@gmail.com>
2021-12-12 18:19:25 +0100

    init tests

---
+++ conftest.py
@@ -0,0 +1,63 @@
+import pytest
+from orders_app.models import Customer, Order, Item, Category
+from django.contrib.auth.models import User
+
+
+@pytest.fixture()
+def user_1(db):
+    return User.objects.create_user('test', 'test@test.com', 'test')
+
+
+@pytest.fixture()
+def new_customer(db):
+    cust = Customer.objects.create(name='test')
+    return cust
+
+
+@pytest.fixture()
+def new_category(db):
+    cat = Category.objects.create(name='test cat')
+    return cat
+
+
+@pytest.fixture()
+def new_item(db):
+    cat = Category.objects.create(name='test cat')
+    itm = Item.objects.create(
+        name='test item',
+        price=10.01,
+        category=cat,
+        unit='szt.'
+    )
+    return itm
+
+
+@pytest.fixture()
+def new_user_factory(db):
+    def create_app_user(
+        username: str,
+        password: str = None,
+        first_name: str = 'firstname',
+        last_name: str = 'lastname',
+        email: str = 'test@test.com',
+        is_staff: bool = False,
+        is_superuser: bool = False,
+        is_active: bool = True,
+    ):
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            is_active=is_active,
+        )
+        return user
+    return create_app_user
+
+
+@pytest.fixture()
+def new_user(db, new_user_factory):
+    return new_user_factory('test user', 'pass', 'name')
---
+++ tests/test_models.py
@@ -0,0 +1,23 @@
+import pytest
+from django.contrib.auth.models import User
+
+
+@pytest.mark.django_db
+def test_user_create(user_1):
+    assert User.objects.count() == 1
+
+
+def test_customer_str(new_customer):
+    assert str(new_customer) == 'test'
+
+
+def test_item_str(new_item):
+    assert str(new_item) == 'test item'
+
+
+def test_category_str(new_category):
+    assert str(new_category) == 'test cat'
+
+
+def test_new_user(new_user):
+    assert new_user.first_name == 'name'
