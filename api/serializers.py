38b1e5948a318ea667479108e65a6537b0396d30
kamilkulerz <kamil.kulerz@gmail.com>
2022-01-03 14:01:31 +0100

    add ordereditem serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,26 +1,34 @@
-from orders_app.models import Category, Item, Customer, Order
+from orders_app.models import Category, Item, Customer, Order, OrderedItem
 from rest_framework import serializers
 
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = ('id', 'name')
 
 
 class ItemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Item
-        fields = ['id', 'name', 'price', 'category', 'unit']
+        fields = ('id', 'name', 'price', 'category', 'unit')
 
 
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Customer
-        fields = ['id', 'name']
+        fields = ('id', 'name')
+
+
+class OrderedItemSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = OrderedItem
+        fields = ('id', 'item', 'quantity', 'status')
 
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
+    items = OrderedItemSerializer(source='ordereditem_set', many=True)
+
     class Meta:
         model = Order
-        fields = ['id', 'customer', 'status']
+        fields = ('id', 'customer', 'status', 'items')
